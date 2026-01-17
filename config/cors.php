13eb71233bb99ff8fc875fbb6356bcb3245a1623
Nghia <trannghia270403@gmail.com>
2026-01-17 16:32:33 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // In production, prefer restricting the allowed origins to your frontend domains.
+    // Example: allow both http and https variants of the frontend domain used by your app.
+    'allowed_origins' => [
+        'http://crm.aaipharma.vn',
+        'https://crm.aaipharma.vn',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -17,6 +22,8 @@ return [
 
     'max_age' => 0,
 
+    // If your frontend needs to send cookies or use `withCredentials`, set this to true.
+    // Note: when supports_credentials is true you must list explicit origins (can't use '*').
     'supports_credentials' => false,
 
 ];
