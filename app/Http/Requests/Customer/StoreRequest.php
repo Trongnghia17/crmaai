5362bae677ed83b4ffd27a9fab215d3a0eb3ea2d
Nghia <trannghia270403@gmail.com>
2026-01-23 13:51:54 +0700

    update

--- app/Http/Requests/Customer/StoreRequest.php
+++ app/Http/Requests/Customer/StoreRequest.php
@@ -23,7 +23,7 @@ class StoreRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'phone' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'max:255', 'unique:customers,phone'],
             'email' => ['nullable', 'email', 'max:255'],
             'address' => ['nullable', 'string', 'max:255'],
             'total_money' => ['nullable', 'numeric'],
@@ -40,6 +40,7 @@ class StoreRequest extends FormRequest
             'phone.required' => 'Số điện thoại không được để trống',
             'phone.string' => 'Số điện thoại phải là chuỗi',
             'phone.max' => 'Số điện thoại không được vượt quá 255 ký tự',
+            'phone.unique' => 'Số điện thoại này đã được sử dụng',
             'email.email' => 'Email không đúng định dạng',
             'email.max' => 'Email không được vượt quá 255 ký tự',
             'address.string' => 'Địa chỉ phải là chuỗi',
